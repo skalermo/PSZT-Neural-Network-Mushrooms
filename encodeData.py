45e505cb287864b3a9a0078d97a8cdb213e3079a
rafalb8 <rafalb8@hotmail.com>
2019-12-27 14:07:48 +0200

    Change to functions

--- encodeData.py
+++ encodeData.py
@@ -1,19 +1,30 @@
 import pandas as pd
+from io import StringIO
 
-# Read file
-data = pd.read_csv('agaricus-lepiota.csv')
 
-# Print Head
-print(data.head(5))
+# Read and encode file
+def encodeFile(input, output):
+    # Read file
+    data = pd.read_csv(input)
 
-# Check for missing values
-for col in data.columns:
-    print('{} : {}'.format(col, data[col].isnull().sum()))
+    # One hot encode data
+    encoded = pd.get_dummies(data)
 
-# One hot encode data
-encoded = pd.get_dummies(data)
+    print('Number of Columns: {} -> {}'.format(len(data.columns), len(encoded.columns)))
 
-print('Number of Columns: {} -> {}'.format(len(data.columns), len(encoded.columns)))
+    # Save to csv file
+    encoded.to_csv(output, index=False)
 
-# Save to csv file
-encoded.to_csv('temp.csv', index=False)
+
+# Read and encode line
+def encodeLine(input):
+    # Prepare input for reading
+    input = StringIO(input)
+
+    # Read input
+    data = pd.read_csv(input, header=None)
+
+    # One hot encode data
+    encoded = pd.get_dummies(data)
+
+    return encoded
