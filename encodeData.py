cf5592bfce1986e153bb220633fe75f57c353eef
rafalb8 <rafalb8@hotmail.com>
2020-01-14 09:10:46 +0100

    Add replace class column values to 0 and 1

--- encodeData.py
+++ encodeData.py
@@ -7,13 +7,16 @@ def encodeFile(input, output):
     # Read file
     data = pd.read_csv(input)
 
+    # Class column 'e' edible = 0, 'p' poisonous = 1
+    data['class'] = data['class'].map({'e' : 0, 'p' : 1})
+
     # One hot encode data
     encoded = pd.get_dummies(data)
 
     print('Number of Columns: {} -> {}'.format(len(data.columns), len(encoded.columns)))
 
+    # Save to csv file
     if output is not None:
-        # Save to csv file
         encoded.to_csv(output, index=False)
 
     return encoded
