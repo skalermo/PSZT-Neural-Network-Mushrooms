176430e530e7a6a98a00e6101dbe18ddd8705186
rafalb8 <rafalb8@hotmail.com>
2020-01-26 23:19:21 +0100

    Update Readme

--- Parser.py
+++ Parser.py
@@ -10,7 +10,6 @@ def parseArgs():
     parser.add_argument('-r', type=float, default=5.0, metavar='', help='Ratio Training to Validation 1:R')
     parser.add_argument('-k', type=float, default=5.0, metavar='', help='Ratio for K-fold cross validation 1:K')
     parser.add_argument('-s', type=float, default=time(), metavar='', help='Random Seed')
-    parser.add_argument('-f', type=str, metavar='', help='Load file')
     parser.add_argument('-c', action='store_true', default=False, help='Output a chart')
     parser.add_argument('-v', action='store_true', default=False, help='Verbose output')
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PSZT-Neural-Network
+## Instrukcja
+Możliwe ustawienia programu:
+ - `-h` Wyświetl możliwe ustawienia programu
+ - `-n` Liczbę neuronów warstwy ukrytej
+ - `-i` Liczba iteracji
+ - `-l` Learning rate
+ - `-r` Stosunek Danych Treningowych do Walidacyjnych 1:R
+ - `-k` Stosunek K-krotna walidacja 1:K
+ - `-s` Ziarno losowości
+ - `-c` Wyświetl wykres
+ - `-v` Tryb ze wszystkimi szczegółami
+ 
+ Użycie programu:
+ 
+        python -m Main [-h] [-n] [-i] [-l] [-r] [-k] [-s] [-c] [-v]
